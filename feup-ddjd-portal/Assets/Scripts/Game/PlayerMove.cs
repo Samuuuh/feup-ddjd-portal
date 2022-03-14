634e188f98887543abd439abc72211b2232f7aba
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2022-03-14 19:10:37 +0000

    new portal and player animation

--- feup-ddjd-portal/Assets/Scripts/Game/PlayerMove.cs
+++ feup-ddjd-portal/Assets/Scripts/Game/PlayerMove.cs
@@ -17,7 +17,7 @@ public class PlayerMove : MonoBehaviour {
     [SerializeField] float miny;
     [SerializeField] float maxy;
 
-
+    public Animator animator;
     public bool facingRight = true;
 
     void Start() {
@@ -25,37 +25,48 @@ public class PlayerMove : MonoBehaviour {
     }
 
     void Update() {
+        // Check if Player is dead
         Dead();
 
+        // Jump
         if (Input.GetKeyDown(KeyCode.Space) && jumpAllowed) {
+            animator.SetFloat("Jump", 1.0f);
+
             rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
             jumpAllowed = false;
-        } else if(!Input.GetKey(KeyCode.Space) && !jumpAllowed) {
+        } else if (!Input.GetKey(KeyCode.Space) && !jumpAllowed) {
             GetComponent<Rigidbody2D>().gravityScale = base_gravity_scale * not_jumping_multiplier;
         }
 
         if (!jumpAllowed && rb.velocity.y < -0.001f) {
             GetComponent<Rigidbody2D>().gravityScale = base_gravity_scale * fall_multiplier;
         }
+
+        // Move Sideways
         if (Input.GetKey(KeyCode.D)) {   
              if(!facingRight) {
-                 transform.eulerAngles = new Vector2(0, 0);
+                transform.eulerAngles = new Vector2(0, 0);
                 FlipPlayer();
             }
 
+            animator.SetFloat("Speed", 1.0f);
             if  (rb.velocity.x < moveSpeed)  rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
         }
         else if (Input.GetKey(KeyCode.A)) {
             if(facingRight) {
-                 transform.eulerAngles = new Vector2(0, 180);
+                transform.eulerAngles = new Vector2(0, 180);
                 FlipPlayer();
             }
 
+            animator.SetFloat("Speed", 1.0f);
             if  (rb.velocity.x > -moveSpeed)  rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
         }
-        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)) rb.velocity = new Vector2(0, rb.velocity.y);
+        else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)) {
+            animator.SetFloat("Speed", 0.0f);
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
         
-
+        // Limit Terminal Velocity
         if (rb.velocity.y < -21f) {
             rb.velocity = new Vector2(rb.velocity.x, -21);
         }
@@ -71,8 +82,29 @@ public class PlayerMove : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other)  {
         if ((other.gameObject.CompareTag("Ground") && !jumpAllowed) || (other.gameObject.CompareTag("SurfaceVer") && !jumpAllowed) || (other.gameObject.CompareTag("SurfaceHor") && !jumpAllowed)) {
+            animator.SetFloat("Jump", 0.0f);
+
             GetComponent<Rigidbody2D>().gravityScale = base_gravity_scale;
             jumpAllowed = true;
+        }
+    }
+
+    // TODO: What to do with Double Jump? 
+    void OnTriggerStay2D(Collider2D other){
+        if ((other.gameObject.CompareTag("Ground") && !jumpAllowed) || (other.gameObject.CompareTag("SurfaceVer") && !jumpAllowed) || (other.gameObject.CompareTag("SurfaceHor") && !jumpAllowed)) {
+            animator.SetFloat("Jump", 0.0f);
+
+            GetComponent<Rigidbody2D>().gravityScale = base_gravity_scale;
+            jumpAllowed = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)  {
+        if ((other.gameObject.CompareTag("Ground") && jumpAllowed) || (other.gameObject.CompareTag("SurfaceVer") && jumpAllowed) || (other.gameObject.CompareTag("SurfaceHor") && jumpAllowed)) {
+            animator.SetFloat("Jump", 1.0f);
+
+            GetComponent<Rigidbody2D>().gravityScale = base_gravity_scale;
+            jumpAllowed = false;
         }
     }
 
