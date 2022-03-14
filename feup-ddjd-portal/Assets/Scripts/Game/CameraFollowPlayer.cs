7ae4b7cf6df2c5dc6d13d201f8e6d87a600f8408
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2022-03-14 19:34:15 +0000

    Clamp the camera

--- feup-ddjd-portal/Assets/Scripts/Game/CameraFollowPlayer.cs
+++ feup-ddjd-portal/Assets/Scripts/Game/CameraFollowPlayer.cs
@@ -2,18 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollowPlayer : MonoBehaviour
-{
+public class CameraFollowPlayer : MonoBehaviour {
     GameObject player;
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    public BoxCollider2D boundBox;
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
+
+    private Camera theCamera;
+    private float halfHeight;
+    private float halfWidth;
+
+    void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
+
+        minBounds = boundBox.bounds.min;
+        maxBounds = boundBox.bounds.max;
+
+        theCamera = GetComponent<Camera>();
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+
+        float clampedX = Mathf.Clamp(transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+        float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
     }
 }
