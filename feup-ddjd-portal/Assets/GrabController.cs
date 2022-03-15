17b67eb7f68d464199069416a691e397065b00ba
hugomguima <hugomguima@gmail.com>
2022-03-15 17:05:31 +0000

    Toggle button for grabbing working

--- feup-ddjd-portal/Assets/GrabController.cs
+++ feup-ddjd-portal/Assets/GrabController.cs
@@ -8,25 +8,43 @@ public class GrabController : MonoBehaviour{
     public Transform boxHolder;
     public float rayDist;
 
+
+    private bool holding = false;
+
     // // Update is called once per frame
     void Update(){
 
 
-        RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, rayDist);
+        RaycastHit2D grabCheckRight = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, rayDist);
+        RaycastHit2D grabCheckLeft = Physics2D.Raycast(grabDetect.position, Vector2.left * transform.localScale, rayDist);
 
-        if(grabCheck.collider != null && grabCheck.collider.tag == "Cube"){
 
-            if(Input.GetKey(KeyCode.E)){
-                    grabCheck.collider.gameObject.transform.parent = boxHolder;
-                    grabCheck.collider.gameObject.transform.position = boxHolder.position;
-                    grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-            }
-            else{
+        if(grabCheckRight.collider != null){
+            ToggleGrab(grabCheckRight);
+        }
+        if(grabCheckLeft.collider != null){
+            ToggleGrab(grabCheckLeft);
+        }
+
+    }
+
+
+    private void ToggleGrab(RaycastHit2D grabCheck){
+
+        if(grabCheck.collider.tag == "Cube" && Input.GetKeyDown(KeyCode.E)){
+
+            if(!holding){
+                grabCheck.collider.gameObject.transform.parent = boxHolder;
+                grabCheck.collider.gameObject.transform.position = boxHolder.position;
+                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                holding = true;
+            }else{
                 grabCheck.collider.gameObject.transform.parent = null;
+                // grabCheck.collider.gameObject.transform.parent = GameObject.Find("Player").transform;
                 grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                holding = false;
             }
-            
-        
+                    
         }
     }
 
