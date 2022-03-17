13d121f8cb086407558916aad25cf405eb7abc09
hugomguima <hugomguima@gmail.com>
2022-03-17 16:04:49 +0000

    almost working cube
    release not working

--- feup-ddjd-portal/Assets/Scripts/GrabController.cs
+++ feup-ddjd-portal/Assets/Scripts/GrabController.cs
@@ -22,43 +22,35 @@ public class GrabController : MonoBehaviour{
         if(grabCheckRight.collider != null){
             ToggleGrab(grabCheckRight);
         }
-        if(grabCheckLeft.collider != null){
+        else if(grabCheckLeft.collider != null){
             ToggleGrab(grabCheckLeft);
         }
-
-
-        
-
-
-        
 
     }
 
 
     private void ToggleGrab(RaycastHit2D grabCheck){
 
-
-        
-
         if(grabCheck.collider.tag == "Cube" && Input.GetKeyDown(KeyCode.E)){
 
             if(!holding){
+
+                Debug.Log("Grabbing");
+
                 grabCheck.collider.gameObject.transform.parent = boxHolder;
                 grabCheck.collider.gameObject.transform.position = boxHolder.position;
                 grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-                // grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0f;
-                // grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().mass = 0f;
-                
                 holding = true;
+
             }else{
+                Debug.Log("Releasing");
                 grabCheck.collider.gameObject.transform.parent = null;
-                // grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().gravityScale = 1f;
                 grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
                 holding = false;
             }
                     
         }
-        // else if(grabCheck.collider.tag == "Cube" && holding){
+        // else if(grabCheck.collider.tag == "CompanionCube" && holding){
         //     grabCheck.collider.gameObject.transform.parent = boxHolder;
         //     grabCheck.collider.gameObject.transform.position = boxHolder.position;
         // }
