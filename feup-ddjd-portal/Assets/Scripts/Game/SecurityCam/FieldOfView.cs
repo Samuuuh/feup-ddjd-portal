e74d499a7cde264252e6d1ddfac76b2c9f328078
hugomguima <hugomguima@gmail.com>
2022-03-21 15:47:56 +0000

    Rotation Done

--- feup-ddjd-portal/Assets/Scripts/Game/SecurityCam/FieldOfView.cs
+++ feup-ddjd-portal/Assets/Scripts/Game/SecurityCam/FieldOfView.cs
@@ -5,20 +5,33 @@ using UnityEngine;
 public class FieldOfView : MonoBehaviour {
     public LayerMask whatIsSolid;
     private Mesh mesh;
-    public int triangleCount;
+    public int triangleCount = 25;
     
+    
+    public float viewDistance = 5f; 
+    public float initialAngle = -45f;
+    public float rotateSpeed = 1f;
+    private float maxAngle = -90;
+    private float minAngle;
+    private bool goingRight = true; 
+
+
+
+
+
     private void Start(){
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
+
+        minAngle = maxAngle + 90;
     }
 
     void Update() {
-        float fov = 90f;
         Vector3 relativePositionVertex = Vector3.zero;
         int rayCount = triangleCount;
-        float angle = 0f;
+        float fov = 90f;
+        float angle = initialAngle;
         float angleIncrease = fov/rayCount;
-        float viewDistance = 5f;
 
         Vector3[] vertices = new Vector3[rayCount + 1 + 1];
         Vector2[] uv = new Vector2[vertices.Length];
@@ -54,6 +67,8 @@ public class FieldOfView : MonoBehaviour {
         mesh.vertices = vertices;
         mesh.uv = uv;
         mesh.triangles = triangles;
+
+        RotateAngle();
     }
 
 
@@ -67,5 +82,25 @@ public class FieldOfView : MonoBehaviour {
 
          if (raycastHit2D.collider.name == name) return true;
         return false;
+    }
+
+    private void RotateAngle(){
+
+        if(goingRight){
+            if (initialAngle + rotateSpeed < minAngle) initialAngle += rotateSpeed;
+            else if(initialAngle + rotateSpeed >= minAngle){
+                initialAngle = minAngle;
+                goingRight = false;
+            } 
+        }
+        else{
+            if (initialAngle - rotateSpeed > maxAngle) initialAngle -= rotateSpeed;
+            else if(initialAngle - rotateSpeed <= maxAngle){
+                initialAngle = maxAngle;
+                goingRight = true; 
+            } 
+        }
+
+        
     }
 }
