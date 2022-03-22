03670da6d1f87d1334ed0e7e8b687f3ffae264e3
hugomguima <hugomguima@gmail.com>
2022-03-22 16:28:03 +0000

    Fixed Patrol Scripts

---
+++ feup-ddjd-portal/Assets/FollowArea.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowArea : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject patrol;
+    
+    // private float directionValue;
+    private float speed = 2f;
+    private Vector3 direction;
+    public bool foundPlayer = false;
+    private bool movingRight = true;
+
+
+
+    void Update(){
+        // if(!foundPlayer) patrol.GetComponent<Patrol>().MoveAimlessly(movingRight);
+
+        if(!foundPlayer){
+            Debug.Log("Moving Aimlessly");
+            Move();
+        }        
+    }
+
+    // Update is called once per frame
+
+    void OnTriggerStay2D(Collider2D collider){
+
+        if(foundPlayer && collider.name == "Player"){
+
+            direction = player.transform.position - patrol.transform.position;
+
+            Debug.Log("Following Player");
+
+            if(direction.x < 0){
+                // Move Left
+                movingRight = false;
+                Move();
+            }
+            else if (direction.x > 0){
+                // Move Right
+                movingRight = true;
+                Move();
+            }
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider){
+        if(collider.name == "Player"){
+            foundPlayer = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider){
+        if(collider.name == "Player"){
+            foundPlayer = false;
+        }
+        else if(collider.name == "Patrol"){
+            Debug.Log("Lost Contact With Patrol");
+            SwapDirection();
+            // movingRight = false;
+        }
+    }
+
+    void SwapDirection(){
+        if(movingRight) movingRight = false;
+        else movingRight = true;
+
+    }
+
+    void Move(){
+        if(movingRight) patrol.transform.Translate(Vector2.right * speed * Time.deltaTime);
+        else patrol.transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+
+}
--- feup-ddjd-portal/Assets/Scripts/Game/Patrol/Patrol.cs
+++ feup-ddjd-portal/Assets/Scripts/Game/Patrol/Patrol.cs
@@ -8,44 +8,112 @@ public class Patrol : MonoBehaviour
     public float speed;
     public float groundDistance = 1f;
     public float wallDistance = 0.125f;
-    private bool movingRight = true;
+    private bool movingRight = true ;
     public Transform groundDetection;
+    public GameObject player;
+    private GameObject followArea;
 
-    // Update is called once per frame
-    void Update(){
+
+    // void Start(){
+    //     followArea = gameObject.transform.GetChild(1).gameObject;
+    // }
+
+
+    // public void MoveAimlessly(bool direction){
+
+    //     movingRight = direction;
         
-        // Move Patrolling unit
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        
+    //     // Change Direction at platform end
+    //     RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, groundDistance);
+    //     RaycastHit2D wallInfoLeft = Physics2D.Raycast(groundDetection.position, Vector2.left, wallDistance);
+    //     RaycastHit2D wallInfoRight = Physics2D.Raycast(groundDetection.position, Vector2.right, wallDistance);
 
-        // Change Direction at platform end
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, groundDistance);
-        RaycastHit2D wallInfoLeft = Physics2D.Raycast(groundDetection.position, Vector2.left, wallDistance);
-        RaycastHit2D wallInfoRight = Physics2D.Raycast(groundDetection.position, Vector2.right, wallDistance);
+    //     Debug.Log(movingRight);
+    //     Debug.Log(wallInfoLeft.collider);
 
-        // Ground Collision
-        if(groundInfo.collider == false){
-            if(movingRight){
-                transform.eulerAngles = new Vector3(0,-180,0);
-                movingRight = false;
-            }
-            else{
-                transform.eulerAngles = new Vector3(0,0,0);
-                movingRight = true;
-            }
-        }
+    //     // Ground Collision
+    //     if(groundInfo.collider == false){
+    //         if(movingRight){
+    //             transform.eulerAngles = new Vector3(0,-180,0);
+    //             movingRight = false;
+    //         }
+    //         else{
+    //             transform.eulerAngles = new Vector3(0,0,0);
+    //             movingRight = true;
+    //         }
+    //     }else{
+            
 
-        // Left Wall Collision
-        else if(!movingRight && wallInfoLeft.collider.tag == "Ground"){
-            transform.eulerAngles = new Vector3(0,0,0);
-            movingRight = true;
-        }
-
-         // Right Wall Collision
-        if(movingRight && wallInfoRight.collider.tag == "Ground"){
-            transform.eulerAngles = new Vector3(0,-180,0);
-            movingRight = false;
-        }
+    //     }
 
 
-    }
+    //     // // Left Wall Collision
+    //     // if(!movingRight && wallInfoLeft.collider.tag == "Ground"){
+    //     //     transform.eulerAngles = new Vector3(0,0,0);
+    //     //     movingRight = true;
+    //     // }
+
+    //     // Left Wall Collision
+    //     if(!movingRight && wallInfoLeft.collider != null){
+    //         if(wallInfoLeft.collider.tag == "Ground"){
+    //             transform.eulerAngles = new Vector3(0,0,0);
+    //             movingRight = true;
+    //         }
+            
+    //     }
+
+    //     // Right Wall Collision
+    //     // if(movingRight && wallInfoRight.collider.tag == "Ground"){
+    //     //     transform.eulerAngles = new Vector3(0,-180,0);
+    //     //     movingRight = false;
+    //     // }
+
+    //     // Move Patrolling unit
+    //     transform.Translate(Vector2.right * speed * Time.deltaTime);
+    // }
+
+
+    // void Update(){
+    //     Move();
+    // }
+
+    // void OnTriggerEnter2D(Collider2D collider){
+    
+    //     // End Game on teacher collision
+    //     if(collider.name == "Player"){
+    //         Debug.Log("TEACHER FOUND YOU. GAME OVER");
+    //     }        
+    // }
+
+
+
+    // void OnTriggerExit2D(Collider2D collider){
+        
+    //     Debug.Log(collider.name);
+
+    //     if(collider.name == "Follow Area"){
+    //         Debug.Log("Exiting collision");
+    //         // Move();
+    //     }
+    // }
+
+    //  void SwapDirection(){
+    //     if(movingRight) movingRight = false;
+    //     else movingRight = true;
+
+    // }
+
+    // void Move(){
+    //     if(movingRight) transform.Translate(Vector2.right * speed * Time.deltaTime);
+    //     else transform.Translate(Vector2.left * speed * Time.deltaTime);
+    // }
+
+
+    // void OnTriggerStayt2D(Collider2D collider){
+        
+    //     Debug.Log(collider.name);
+
+
+    // }
 }
