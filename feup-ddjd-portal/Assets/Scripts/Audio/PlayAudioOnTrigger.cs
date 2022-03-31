06a8d75196ddc2855432e032fc0f4da19616c8f2
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2022-03-31 15:04:30 +0100

    Added Audio

---
+++ feup-ddjd-portal/Assets/Scripts/Audio/PlayAudioOnTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class PlayAudioOnTrigger: MonoBehaviour {
+    [SerializeField] private AudioSource myAudio;
+
+    void Start() {
+
+    }
+
+
+    void Update() {
+
+    }
+}
---
+++ feup-ddjd-portal/Assets/Scripts/Game/Player/PlayOnPlayerCollider.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnPlayerCollider : MonoBehaviour {
+    [SerializeField] private string soundName;
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.tag == "Player") {
+            FindObjectOfType<AudioManager>().Play(soundName);
+            Destroy(gameObject);
+        }
+    }
+}
