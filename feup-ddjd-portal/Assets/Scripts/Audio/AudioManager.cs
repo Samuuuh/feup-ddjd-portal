e951cbc989e0fef0a85c8d1c68f1ad7a0db53f19
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2022-03-31 19:51:39 +0100

    Audio manager working fine

--- feup-ddjd-portal/Assets/Scripts/Audio/AudioManager.cs
+++ feup-ddjd-portal/Assets/Scripts/Audio/AudioManager.cs
@@ -5,19 +5,45 @@ using UnityEngine;
 
 public class AudioManager: MonoBehaviour {
     public AudioSettings audioSettings;
+
+    public bool ambientMusicOn;
+    public Sound ambientMusic;
     public Sound[] sounds;
 
-    void Awake() {
+    private AudioSource audioPlaying;
+    private float defaultVolume;
+    private float lowerVolume;
+
+    private void Awake() {
+        defaultVolume = audioSettings.GetInstance().volume;
+        lowerVolume = defaultVolume - defaultVolume * (2f/3f);
+
         foreach (Sound s in sounds) {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
 
-            s.source.volume = audioSettings.GetInstance().volume;
+            s.source.volume = defaultVolume;
+        }
+
+        if (ambientMusicOn) {
+            ambientMusic.source = gameObject.AddComponent<AudioSource>();
+            ambientMusic.source.volume = defaultVolume;
+            ambientMusic.source.clip = ambientMusic.clip;
+            ambientMusic.source.Play();
+        }
+    }
+
+    private void Update() {
+        if (!audioPlaying.isPlaying) {
+            ambientMusic.source.volume = defaultVolume;
         }
     }
 
     public void Play(string name) {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.Play();
+
+        ambientMusic.source.volume = lowerVolume;
+        audioPlaying = s.source;
     }
 }
